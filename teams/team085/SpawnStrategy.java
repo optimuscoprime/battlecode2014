be709e144d871d23f2b9c3f221b95309a8178521
Rupert Shuttleworth <optimuscoprime@gmail.com>
2014-01-14 12:57:04 +1100

    make gk_master as team085

---
+++ teams/team085/SpawnStrategy.java
@@ -0,0 +1,50 @@
+package team085;
+
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.GameConstants;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+import battlecode.common.RobotInfo;
+
+/**
+ * HQ - continuous spawn strategy, while attacking nearby enemy robots
+ */
+public class SpawnStrategy implements Strategy {
+	RobotController rc;
+	RobotInfo INFO;
+	MapLocation HQ_LOCATION;
+	Direction DIRECTION_TO_ENEMY;
+	MapLocation PRIMARY_SPAWN_LOCATION;
+	
+	public SpawnStrategy(RobotController rc) throws GameActionException {
+		this.rc = rc;
+		INFO = rc.senseRobotInfo(rc.getRobot());
+		HQ_LOCATION = rc.senseHQLocation();
+		DIRECTION_TO_ENEMY = this.HQ_LOCATION.directionTo(rc.senseEnemyHQLocation());
+		PRIMARY_SPAWN_LOCATION = HQ_LOCATION.add(DIRECTION_TO_ENEMY);
+	}
+	
+	public void play() throws GameActionException {
+		if (rc.isActive()) {
+			Tactics.killNearbyEnemies(rc, INFO);
+			spawnRobot();
+		}
+	}
+	
+	public void spawnRobot() throws GameActionException {
+		if (rc.senseRobotCount() < GameConstants.MAX_ROBOTS) {
+			if (rc.senseObjectAtLocation(PRIMARY_SPAWN_LOCATION) == null) {
+				rc.spawn(DIRECTION_TO_ENEMY);
+			} else {
+				for (Direction d : Navigation.DIRECTIONS) {
+					MapLocation loc = HQ_LOCATION.add(d);
+					if (rc.senseObjectAtLocation(loc) == null) {
+						rc.spawn(d);
+						break;
+					}
+				}				
+			}
+		}
+	}
+}
---
+++ teams/team085/Strategy.java
@@ -0,0 +1,7 @@
+package team085;
+
+import battlecode.common.GameActionException;
+
+public interface Strategy {
+	public void play() throws GameActionException;
+}
