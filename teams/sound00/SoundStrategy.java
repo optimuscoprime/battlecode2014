9b51fad387d96c03a9b505eabd33341e5965256b
Ian <ian.goldsmith@gmail.com>
2014-01-15 23:06:53 +1100

    Far more meaningful demo of what sound towers can do. This should get fun soon.

--- teams/sound00/SoundStrategy.java
+++ teams/sound00/SoundStrategy.java
@@ -22,7 +22,9 @@ public class SoundStrategy implements Strategy {
 	Team ENEMY;
 	RobotInfo INFO;
 	Direction wanderingDirection;
-	MapLocation target;
+	MapLocation firstTarget;
+	MapLocation currentTarget;
+	Direction DIRECTION_TO_HQ;
 
 	public SoundStrategy(RobotController rc, Random rand) throws GameActionException {
 		this.rc = rc;
@@ -33,18 +35,25 @@ public class SoundStrategy implements Strategy {
 		//for now lets actually just start at the max range closest to the enemyhq
 		// and 'pulse' towards our HQ.
 		Direction DIRECTION_TO_ENEMY = INFO.location.directionTo(rc.senseEnemyHQLocation());
+		DIRECTION_TO_HQ = DIRECTION_TO_ENEMY.opposite();
 		// lets make a function that generates the line between A-B where A is closest enemy hq.
 		// B is closest friendly HQ.
-		target=rc.getLocation();
-		MapLocation furtherTarget=target.add(DIRECTION_TO_ENEMY);
+		firstTarget=rc.getLocation();
+		MapLocation furtherTarget=firstTarget.add(DIRECTION_TO_ENEMY);
 		while(rc.canAttackSquare(furtherTarget)){
-			target=furtherTarget;
+			firstTarget=furtherTarget;
 			furtherTarget=furtherTarget.add(DIRECTION_TO_ENEMY);
 		}
+		currentTarget=firstTarget;
 	}
 	
 	public void play() throws GameActionException {
-			  rc.attackSquare(target);
+		if(!rc.canAttackSquare(currentTarget)){
+			currentTarget=firstTarget;
+		}
+			  rc.attackSquare(currentTarget);
+			  currentTarget=currentTarget.add(DIRECTION_TO_HQ);
+			  currentTarget=currentTarget.add(DIRECTION_TO_HQ);
 	}
 }
 
