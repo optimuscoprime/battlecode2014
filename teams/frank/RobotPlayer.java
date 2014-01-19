c8cf5bfe2bac647734cabc721e02a441cf4cb110
Rupert Shuttleworth <optimuscoprime@gmail.com>
2014-01-19 16:20:52 +1100

    frank

--- teams/frank/RobotPlayer.java
+++ teams/frank/RobotPlayer.java
@@ -1,10 +1,57 @@
 package frank;
 
-import battlecode.common.*;
 import java.util.*;
 
-public class RobotPlayer {
-    public static void run(RobotController rc) {
+import battlecode.common.*;
+static import battlecode.common.RobotType.*;
 
+public class RobotPlayer {
+    private static final RobotController rc;
+
+    private static void die() {
+        // asserts are disabled by default
+        printf("Die\n");
+        System.exit(-1);
+    }
+
+    public static void run (RobotController rc) {
+        this.rc = rc;
+        while (true) {
+            playMove();
+            rc.yield();
+        }
+    }
+
+    private static void playMove() {
+        switch (rc.getType()) {
+            case HQ:
+                playHqMove();
+                break;
+            case NOISETOWER:
+                playNoiseTowerMove();
+                break;
+            case PASTR:
+                playPastrMove();
+                break;
+            case SOLDIER:
+                playSoldierMove();
+                break;
+        }
+    }
+
+    private static void playHqMove() {
+        die();
+    }
+
+    private static void playNoiseTowerMove() {
+        die();
+    }
+
+    private static void playPastrMove() {
+        die();
+    }
+
+    private static void playSoldierMove() {
+        die();
     }
 }
